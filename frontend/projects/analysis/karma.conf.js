50260168185db4ca66fb91659ae47991770e1079
Gérald Pereira <gerald.pereira@octoperf.com>
2019-07-11 16:00:57 +0200

    Add json karma report

--- frontend/projects/analysis/karma.conf.js
+++ frontend/projects/analysis/karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../../coverage/analysis'),
-      reports: ['html', 'lcovonly'],
+      reports: ['html', 'lcovonly', 'json'],
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
