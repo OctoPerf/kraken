e17c2775cb8e1c1db2db9f785a3ec293fadd88d1
Gérald Pereira <gerald.pereira@octoperf.com>
2019-11-09 23:58:28 +0100

    Unit tests

--- backend/commons/runtime/runtime-docker/src/test/java/com/kraken/runtime/docker/DockerTaskServiceTest.java
+++ backend/commons/runtime/runtime-docker/src/test/java/com/kraken/runtime/docker/DockerTaskServiceTest.java
@@ -5,11 +5,13 @@ import com.google.common.collect.ImmutableMap;
 import com.kraken.runtime.command.Command;
 import com.kraken.runtime.command.CommandService;
 import com.kraken.runtime.docker.entity.DockerContainer;
+import com.kraken.runtime.docker.entity.DockerContainerTest;
 import com.kraken.runtime.docker.env.EnvironmentChecker;
 import com.kraken.runtime.docker.env.EnvironmentPublisher;
 import com.kraken.runtime.docker.properties.DockerPropertiesTest;
 import com.kraken.runtime.entity.LogType;
 import com.kraken.runtime.entity.Task;
+import com.kraken.runtime.entity.TaskTest;
 import com.kraken.runtime.entity.TaskType;
 import com.kraken.runtime.logs.LogsService;
 import com.kraken.tools.unique.id.IdGenerator;
@@ -19,17 +21,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.GroupedFlux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DockerTaskServiceTest {
@@ -84,6 +89,8 @@ public class DockerTaskServiceTest {
 
     given(idGenerator.generate()).willReturn(taskId);
     given(envChecker.test(taskType)).willReturn(true);
+    given(envPublisher.test(taskType)).willReturn(true);
+    given(envPublisher.get()).willReturn(ImmutableMap.of("FOO", "BAR"));
     given(taskTypeToPath.apply(taskType)).willReturn(path);
 
     final var executeCmd = Command.builder()
@@ -94,7 +101,9 @@ public class DockerTaskServiceTest {
             "-d",
             "--no-color"))
         .environment(ImmutableMap.<String, String>builder().putAll(env)
-            .put("KRAKEN_TASK_ID", taskId).build())
+            .put("KRAKEN_TASK_ID", taskId)
+            .put("FOO", "BAR")
+            .build())
         .build();
 
     final var logs = Flux.just("logs");
@@ -105,6 +114,62 @@ public class DockerTaskServiceTest {
 
     verify(commandService).execute(executeCmd);
     verify(logsService).push(applicationId, taskId, LogType.TASK, logs);
-
   }
+
+  @Test
+  public void shouldCancel() {
+    final var applicationId = "applicationId";
+    final var taskId = TaskTest.TASK.getId();
+    final var taskType = TaskType.RUN;
+    final var path = "path";
+
+    given(envPublisher.test(taskType)).willReturn(true);
+    given(envPublisher.get()).willReturn(ImmutableMap.of("FOO", "BAR"));
+    given(taskTypeToPath.apply(taskType)).willReturn(path);
+
+    final var cancelCmd = Command.builder()
+        .path(taskTypeToPath.apply(taskType))
+        .command(Arrays.asList("docker-compose",
+            "--no-ansi",
+            "down"))
+        .environment(ImmutableMap.<String, String>builder()
+            .put("KRAKEN_TASK_ID", taskId)
+            .put("FOO", "BAR")
+            .build())
+        .build();
+
+    final var logs = Flux.just("logs");
+    given(commandService.execute(cancelCmd)).willReturn(logs);
+    given(logsService.concat(logs)).willReturn(logs);
+
+    assertThat(service.cancel(applicationId, TaskTest.TASK).block()).isEqualTo(taskId);
+
+    verify(commandService).execute(cancelCmd);
+    verify(logsService).push(applicationId, taskId, LogType.TASK, logs);
+  }
+
+  @Test
+  public void shouldList() {
+    final var task = TaskTest.TASK;
+
+    final var listCommand = Command.builder()
+        .path(".")
+        .command(Arrays.asList("docker",
+            "ps",
+            "-a",
+            "--filter", "label=com.kraken.taskId",
+            "--format", StringToDockerContainer.FORMAT))
+        .environment(ImmutableMap.of())
+        .build();
+
+    final var taskAsString = "taskAsString";
+    given(commandService.execute(listCommand)).willReturn(Flux.just(taskAsString));
+    given(stringToDockerContainer.apply(taskAsString)).willReturn(DockerContainerTest.CONTAINER);
+    given(dockerContainersToTask.apply(any())).willReturn(Mono.just(task));
+
+    assertThat(service.list().blockLast()).isEqualTo(task);
+
+    verify(commandService).execute(listCommand);
+  }
+
 }
