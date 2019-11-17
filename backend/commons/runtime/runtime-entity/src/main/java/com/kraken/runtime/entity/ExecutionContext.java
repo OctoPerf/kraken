946ce74fa92528972a258dd801d2081380ec5f0f
Gérald Pereira <gerald.pereira@octoperf.com>
2019-11-17 17:00:06 +0100

    Execution context

--- backend/commons/runtime/runtime-entity/src/main/java/com/kraken/runtime/entity/ExecutionContext.java
+++ backend/commons/runtime/runtime-entity/src/main/java/com/kraken/runtime/entity/ExecutionContext.java
@@ -1,19 +1,40 @@
-package com.kraken.runtime.kubernetes.deployment;
+package com.kraken.runtime.entity;
 
-import com.kraken.runtime.entity.TaskType;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
-import lombok.NonNull;
 import lombok.Value;
+import lombok.With;
 
 import java.util.Map;
 
+import static java.util.Objects.requireNonNull;
+
 @Value
 @Builder
-public class DeploymentContext {
+public class ExecutionContext {
+  @With
+  String applicationId;
+  @With
+  String taskId;
+  TaskType taskType;
+  String description;
+  Map<String, String> environment;
+  //  Key: hostId, Value; env specific to this host
+  Map<String, Map<String, String>> hosts;
 
-  @NonNull String taskId;
-  @NonNull String applicationId;
-  @NonNull TaskType taskType;
-  @NonNull Map<String, String> environment;
-  @NonNull Integer replicas;
+  @JsonCreator
+  ExecutionContext(
+      @JsonProperty("description") final String description,
+      @JsonProperty("taskType") final TaskType taskType,
+      @JsonProperty("environment") final Map<String, String> environment,
+      @JsonProperty("hosts") final Map<String, Map<String, String>> hosts) {
+    super();
+    this.applicationId = "";
+    this.taskId = "";
+    this.description = requireNonNull(description);
+    this.taskType = requireNonNull(taskType);
+    this.environment = requireNonNull(environment);
+    this.hosts = requireNonNull(hosts);
+  }
 }
--- backend/commons/runtime/runtime-entity/src/test/java/com/kraken/runtime/entity/ExecutionContextTest.java
+++ backend/commons/runtime/runtime-entity/src/test/java/com/kraken/runtime/entity/ExecutionContextTest.java
@@ -2,13 +2,22 @@ package com.kraken.runtime.entity;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import lombok.NonNull;
 import org.junit.Test;
+
+import java.util.Map;
 
 import static com.kraken.test.utils.TestUtils.shouldPassAll;
 
-public class HostTest {
+public class ExecutionContextTest {
 
-  public static final Host HOST = Host.builder()
+//  @NonNull String applicationId;
+//  @NonNull TaskType taskType;
+//  @NonNull Map<String, String> environment;
+//  //  Key: hostId, Value; env specific to this host
+//  @NonNull Map<String, Map<String, String>> hosts;
+
+  public static final ExecutionContext EXECUTION_CONTEXT = ExecutionContext.builder()
       .id("hostId")
       .name("hostName")
       .capacity(ImmutableMap.of())
