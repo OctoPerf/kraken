6eca055798a87e50cedc1dc4ce7b39fffab3ac4e
Gérald Pereira <gerald.pereira@octoperf.com>
2019-11-29 14:49:28 +0100

    Tasks table

--- frontend/projects/runtime/src/lib/runtime-task/runtime-task.module.ts
+++ frontend/projects/runtime/src/lib/runtime-task/runtime-task.module.ts
@@ -2,13 +2,24 @@ import {NgModule} from '@angular/core';
 import {RuntimeContainerService} from 'projects/runtime/src/lib/runtime-task/runtime-container.service';
 import {RuntimeTaskService} from 'projects/runtime/src/lib/runtime-task/runtime-task.service';
 import {TaskTableComponent} from './task-table/task-table.component';
+import {ComponentsModule} from 'projects/components/src/lib/components.module';
+import {VendorsModule} from 'projects/vendors/src/lib/vendors.module';
+import {IconModule} from 'projects/icon/src/lib/icon.module';
+import {DateModule} from 'projects/date/src/lib/date.module';
+import {ToolsModule} from 'projects/tools/src/lib/tools.module';
 
 
 @NgModule({
   declarations: [
     TaskTableComponent
   ],
-  imports: [],
+  imports: [
+    ComponentsModule,
+    VendorsModule,
+    IconModule,
+    DateModule,
+    ToolsModule,
+  ],
   exports: [
     TaskTableComponent,
   ],
--- frontend/projects/runtime/src/lib/runtime-task/task-table/task-table.component.ts
+++ frontend/projects/runtime/src/lib/runtime-task/task-table/task-table.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {SelectionModel} from '@angular/cdk/collections';
+import {Task} from 'projects/runtime/src/lib/entities/task';
+import {REFRESH_ICON} from 'projects/icon/src/lib/icons';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatSort} from '@angular/material/sort';
+import {Subscription} from 'rxjs';
+import {RuntimeTaskService} from 'projects/runtime/src/lib/runtime-task/runtime-task.service';
+import {EventBusService} from 'projects/event/src/lib/event-bus.service';
+import {TasksRefreshEvent} from 'projects/runtime/src/lib/events/tasks-refresh-event';
+import {map} from 'rxjs/operators';
+import {TaskSelectedEvent} from 'projects/runtime/src/lib/events/task-selected-event';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'lib-task-table',
   templateUrl: './task-table.component.html',
   styleUrls: ['./task-table.component.scss']
 })
-export class TaskTableComponent implements OnInit {
+export class TaskTableComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private readonly _selection: SelectionModel<Task> = new SelectionModel(false);
+  private readonly _subscriptions: Subscription[] = [];
+  readonly refreshIcon = REFRESH_ICON;
+  readonly displayedColumns: string[] = [/*'id',*/ 'startDate', 'status', 'type', 'containers', 'descriptions'];
+  loading = true;
+  dataSource: MatTableDataSource<Task>;
+
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  constructor(private taskService: RuntimeTaskService,
+              private eventBus: EventBusService) {
+    this._subscriptions.push( eventBus.of<TasksRefreshEvent>(TasksRefreshEvent.CHANNEL)
+      .pipe(map(event => event.tasks)).subscribe(tasks => this.tasks = tasks));
+    this._subscriptions.push(this._selection.changed.subscribe(value => {
+      this.eventBus.publish(new TaskSelectedEvent(this._selection.selected[0]));
+    }));
+  }
 
   ngOnInit() {
+    this.refresh();
+  }
+
+  ngOnDestroy() {
+    _.invokeMap(this._subscriptions, 'unsubscribe');
+  }
+
+  refresh() {
+    this.loading = true;
+    this.taskService.list().subscribe();
+  }
+
+  set tasks(tasks: Task[]) {
+    this.dataSource = new MatTableDataSource(tasks);
+    this.dataSource.sort = this.sort;
+    this.loading = false;
   }
 
 }
