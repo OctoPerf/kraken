83f52176d208df76e0817311b40238baeb754aff
Gérald Pereira <gerald.pereira@octoperf.com>
2019-12-18 23:03:30 +0100

    Host refactor

---
+++ backend/commons/runtime/runtime-docker/src/test/java/com/kraken/runtime/docker/DockerHostServiceTest.java
@@ -0,0 +1,34 @@
+package com.kraken.runtime.docker;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.kraken.runtime.entity.Host;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DockerHostServiceTest {
+
+  DockerHostService service;
+
+  @Before
+  public void before() {
+    service = new DockerHostService();
+  }
+
+  @Test
+  public void shouldList() {
+
+    final var list = service.list().collectList().block();
+    assertThat(list).isNotNull();
+    assertThat(list.size()).isEqualTo(1);
+    assertThat(list.get(0)).isEqualTo(Host.builder()
+        .id("local")
+        .name("local")
+        .capacity(ImmutableMap.of())
+        .addresses(ImmutableList.of())
+        .build());
+  }
+
+}
