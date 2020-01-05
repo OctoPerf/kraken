29ede5d0abc7a8010244d1ec466d625b99213c90
Gérald Pereira <gerald.pereira@octoperf.com>
2020-01-05 23:33:07 +0100

    Doc update

--- backend/commons/tools/environment/src/main/java/com/kraken/tools/environment/KrakenEnvironmentLabels.java
+++ backend/commons/tools/environment/src/main/java/com/kraken/tools/environment/KrakenEnvironmentLabels.java
@@ -3,7 +3,7 @@ package com.kraken.tools.environment;
 public interface KrakenEnvironmentLabels {
 
   String COM_KRAKEN_TASK_ID = "com.kraken/taskId";
-  String COM_KRAKEN_HOST_ID = "com.kraken/hostId";
+  String COM_KRAKEN_HOST_ID = "sses";
   String COM_KRAKEN_TASK_TYPE = "com.kraken/taskType";
   String COM_KRAKEN_APPLICATION_ID = "com.kraken/applicationId";
   String COM_KRAKEN_DESCRIPTION = "com.kraken/description";
