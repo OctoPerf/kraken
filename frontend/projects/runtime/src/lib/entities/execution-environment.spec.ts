82bd4d206081b62f3388abaa29b66bf28838d983
Gérald Pereira <gerald.pereira@octoperf.com>
2020-03-10 14:18:15 +0100

    lint

--- frontend/projects/runtime/src/lib/entities/execution-environment.spec.ts
+++ frontend/projects/runtime/src/lib/entities/execution-environment.spec.ts
@@ -1,20 +1,20 @@
 import {ExecutionEnvironment} from 'projects/runtime/src/lib/entities/execution-environment';
 import {ExecutionEnvironmentEntry} from 'projects/runtime/src/lib/entities/execution-environment-entry';
 
-export const testExecutionEnvironment: () => ExecutionEnvironment = () => {
-  return new ExecutionEnvironment(
-    'GATLING_RUN',
-    'description',
-    ['local'],
-    [testExecutionEnvironmentEntry()],
-  );
-};
-
 export const testExecutionEnvironmentEntry: () => ExecutionEnvironmentEntry = () => {
   return new ExecutionEnvironmentEntry(
     '',
     'USER',
     'key',
     'value',
+  );
+};
+
+export const testExecutionEnvironment: () => ExecutionEnvironment = () => {
+  return new ExecutionEnvironment(
+    'GATLING_RUN',
+    'description',
+    ['local'],
+    [testExecutionEnvironmentEntry()],
   );
 };
