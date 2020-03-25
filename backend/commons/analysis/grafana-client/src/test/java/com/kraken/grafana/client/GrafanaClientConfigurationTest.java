9ee99a4b2b088f2ae48beed3b0d6154036687481
jloisel <loisel.jerome@gmail.com>
2020-03-25 18:17:18 +0100

    fix build

--- backend/commons/analysis/grafana-client/src/test/java/com/kraken/grafana/client/GrafanaClientConfigurationTest.java
+++ backend/commons/analysis/grafana-client/src/test/java/com/kraken/grafana/client/GrafanaClientConfigurationTest.java
@@ -1,20 +1,17 @@
 package com.kraken.grafana.client;
 
-import com.kraken.grafana.client.properties.GrafanaClientPropertiesTestConfiguration;
+import com.kraken.Application;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration(
-    classes = {GrafanaClientConfiguration.class, GrafanaClientPropertiesTestConfiguration.class},
-    initializers = {ConfigFileApplicationContextInitializer.class})
+@SpringBootTest(classes = Application.class)
 public class GrafanaClientConfigurationTest {
 
   @Qualifier("webClientGrafana")
