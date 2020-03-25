184b1dcda4ea73796d80d87ce6a9930898300763
jloisel <loisel.jerome@gmail.com>
2020-03-25 16:34:17 +0100

    Enable configuration properties beans

--- backend/commons/tools/application/src/main/java/com/kraken/Application.java
+++ backend/commons/tools/application/src/main/java/com/kraken/Application.java
@@ -2,13 +2,13 @@ package com.kraken;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.kraken"})
+@EnableConfigurationProperties
 public class Application {
 
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     SpringApplication.run(Application.class, args);
   }
 
