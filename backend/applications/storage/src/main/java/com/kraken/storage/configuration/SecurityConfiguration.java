628edda2c0721c0950ff6a4ca94082df796faea0
Gérald Pereira <gerald.pereira@octoperf.com>
2020-03-25 19:07:41 +0100

    Secure files/** endpoints

--- backend/applications/storage/src/main/java/com/kraken/storage/configuration/SecurityConfiguration.java
+++ backend/applications/storage/src/main/java/com/kraken/storage/configuration/SecurityConfiguration.java
@@ -2,12 +2,16 @@ package com.kraken.storage.configuration;
 
 // https://blog.ineat-group.com/2019/01/securiser-vos-apis-spring-webflux-avec-keycloak-et-oauth2/
 
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
@@ -15,6 +19,8 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 import reactor.core.publisher.Mono;
 
 import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @EnableWebFluxSecurity
 public class SecurityConfiguration {
@@ -24,7 +30,10 @@ public class SecurityConfiguration {
         .authorizeExchange()
         .pathMatchers("/static/**")
         .permitAll()
-        .anyExchange().authenticated()
+        .pathMatchers(HttpMethod.OPTIONS).permitAll()
+        .pathMatchers("/files/**").hasAuthority("USER")
+        .pathMatchers("/files/**").hasAuthority("ADMIN")
+        .anyExchange().denyAll()
         .and()
         .oauth2ResourceServer()
         .jwt()
@@ -32,30 +41,22 @@ public class SecurityConfiguration {
     return http.build();
   }
 
+  // Todo move to sec module
   @Bean
   Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
-    GrantedAuthoritiesExtractor extractor = new GrantedAuthoritiesExtractor();
+    JwtAuthenticationConverter extractor = new JwtAuthenticationConverter();
+    extractor.setJwtGrantedAuthoritiesConverter(jwt -> {
+      final var claims = jwt.getClaims();
+      JSONObject realmAccess = (JSONObject) claims.get("realm_access");
+      JSONArray roles = (JSONArray) realmAccess.get("roles");
+      JSONArray groups = (JSONArray) claims.get("user_groups");
+      // TODO where to put this in order to get them in the AuthenticationPrincipal ?
+      System.out.println(groups);
+      return roles.stream()
+          .map(Object::toString)
+          .map(SimpleGrantedAuthority::new)
+          .collect(Collectors.toSet());
+    });
     return new ReactiveJwtAuthenticationConverterAdapter(extractor);
-  }
-
-  static class GrantedAuthoritiesExtractor extends JwtAuthenticationConverter {
-    @Override
-    protected Collection<GrantedAuthority> extractAuthorities(Jwt jwt) {
-      System.out.println(jwt.getClaims());
-      System.out.println(jwt.getHeaders());
-//      {sub=db5d1288-9447-4742-b53e-63fcd0564e01, resource_access={"account":{"roles":["manage-account","manage-account-links","view-profile"]}}, email_verified=true, iss=http://localhost:9080/auth/realms/kraken, groups=[], typ=Bearer, preferred_username=kraken-admin, aud=[account], acr=1, realm_access={"roles":["offline_access","uma_authorization","ADMIN"]}, azp=kraken-web, scope=email profile, exp=2020-03-25T16:58:05Z, session_state=1368e338-30f3-43ad-91d0-d2aff9581885, iat=2020-03-25T16:53:05Z, jti=97999b83-551e-4ae4-85f1-4cf20d6f6a77}
-//        {kid=Yo_KOb3yFgpis3NmOQv8Q47fIBYmnJldKeDMKCYAA8c, typ=JWT, alg=RS256}
-
-//      {sub=2e44ffae-111c-4f59-ae2b-65000de6f7b7, resource_access={"account":{"roles":["manage-account","manage-account-links","view-profile"]}}, email_verified=true, iss=http://localhost:9080/auth/realms/kraken, groups=[], typ=Bearer, preferred_username=kraken-user, aud=[account], acr=1, realm_access={"roles":["offline_access","uma_authorization","USER"]}, azp=kraken-web, scope=email profile, exp=2020-03-25T16:59:48Z, session_state=d7f2cf49-48bf-4637-8882-c24fe7a59bf8, iat=2020-03-25T16:54:48Z, jti=06f6b8b1-b56f-4712-9295-b66f8fe60252}
-//        {kid=Yo_KOb3yFgpis3NmOQv8Q47fIBYmnJldKeDMKCYAA8c, typ=JWT, alg=RS256}
-
-//      {sub=2e44ffae-111c-4f59-ae2b-65000de6f7b7, resource_access={"account":{"roles":["manage-account","manage-account-links","view-profile"]}}, email_verified=true, iss=http://localhost:9080/auth/realms/kraken, groups=[], typ=Bearer, preferred_username=kraken-user, aud=[account], acr=1, realm_access={"roles":["offline_access","uma_authorization","USER"]}, azp=kraken-web, scope=email profile, exp=2020-03-25T16:59:48Z, session_state=d7f2cf49-48bf-4637-8882-c24fe7a59bf8, iat=2020-03-25T16:54:48Z, jti=06f6b8b1-b56f-4712-9295-b66f8fe60252}
-//        {kid=Yo_KOb3yFgpis3NmOQv8Q47fIBYmnJldKeDMKCYAA8c, typ=JWT, alg=RS256}
-
-//      {sub=2e44ffae-111c-4f59-ae2b-65000de6f7b7, resource_access={"account":{"roles":["manage-account","manage-account-links","view-profile"]}}, email_verified=true, iss=http://localhost:9080/auth/realms/kraken, groups=[], typ=Bearer, preferred_username=kraken-user, aud=[account], acr=1, realm_access={"roles":["offline_access","uma_authorization","USER"]}, azp=kraken-web, scope=email profile, exp=2020-03-25T16:59:48Z, session_state=d7f2cf49-48bf-4637-8882-c24fe7a59bf8, iat=2020-03-25T16:54:48Z, jti=06f6b8b1-b56f-4712-9295-b66f8fe60252}
-//        {kid=Yo_KOb3yFgpis3NmOQv8Q47fIBYmnJldKeDMKCYAA8c, typ=JWT, alg=RS256}
-
-        return super.extractAuthorities(jwt);
-    }
   }
 }
--- development/README.md
+++ development/README.md
@@ -68,10 +68,18 @@ docker stop grafana
 
 ## Keycloak tests
 
+
+User
 ```
 curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'username=kraken-user&password=kraken&grant_type=password' -d 'client_id=kraken-web' "http://localhost:9080/auth/realms/kraken/protocol/openid-connect/token" | jq -r '.access_token' > token
 ```
 
+Admin
+```
+curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'username=kraken-admin&password=kraken&grant_type=password' -d 'client_id=kraken-web' "http://localhost:9080/auth/realms/kraken/protocol/openid-connect/token" | jq -r '.access_token' > token
+```
+
+List files
 ```
 curl --verbose -X GET http://localhost:8080/files/list -H "Authorization: Bearer $(cat token)"
 ```
