533f6fa234d9bd19798cf3871df18366941b3242
jloisel <loisel.jerome@gmail.com>
2020-03-27 09:34:08 +0100

    remove env values

--- backend/commons/tools/environment/src/main/java/com/kraken/tools/environment/KrakenEnvironmentAtValues.java
+++ backend/commons/tools/environment/src/main/java/com/kraken/tools/environment/KrakenEnvironmentAtValues.java
@@ -1,16 +1,6 @@
 package com.kraken.tools.environment;
 
 public interface KrakenEnvironmentAtValues {
-  String $KRAKEN_ANALYSIS_RESULTS_ROOT = "${kraken.analysis.results.root}";
-
-  String $KRAKEN_K8S_NAMESPACE = "${kraken.k8s.namespace:#{environment.KRAKEN_K8S_NAMESPACE}}";
-  String $KRAKEN_K8S_PRETTY = "${kraken.k8s.pretty:#{environment.KRAKEN_K8S_PRETTY}}";
-  String $KRAKEN_K8S_DEBUG = "${kraken.k8s.debug:#{environment.KRAKEN_K8S_DEBUG}}";
-  String $KRAKEN_K8S_PATCH_HOSTS = "${kraken.k8s.patch-hosts:#{environment.KRAKEN_K8S_PATCH_HOSTS}}";
-  String $KRAKEN_K8S_CLIENT_BUILDER_TYPE = "${kraken.k8s.client-builder-type:#{environment.KRAKEN_K8S_CLIENT_BUILDER_TYPE}}";
-  String $KRAKEN_K8S_CONFIG_PATH = "${kraken.k8s.config-path:#{environment.KRAKEN_K8S_CONFIG_PATH}}";
-  String $KRAKEN_VERSION = "${kraken.version:#{environment.KRAKEN_VERSION}}";
-
   String $KRAKEN_GATLING_HOME = "${kraken.gatling.home:#{environment.KRAKEN_GATLING_HOME}}";
   String $KRAKEN_GATLING_BIN = "${kraken.gatling.bin:#{environment.KRAKEN_GATLING_BIN}}";
   String $KRAKEN_GATLING_CONF_LOCAL = "${kraken.gatling.conf.local:#{environment.KRAKEN_GATLING_CONF_LOCAL}}";
@@ -30,8 +20,6 @@ public interface KrakenEnvironmentAtValues {
   String $KRAKEN_GATLING_SIMULATION_PACKAGE = "${kraken.gatling.simulation-package:#{environment.KRAKEN_GATLING_SIMULATION_PACKAGE}}";
   String $KRAKEN_GATLING_SIMULATION_CLASS = "${kraken.gatling.simulation-class:#{environment.KRAKEN_GATLING_SIMULATION_CLASS}}";
   String $KRAKEN_GATLING_JAVA_OPTS = "${kraken.gatling.java-opts:#{environment.KRAKEN_GATLING_JAVA_OPTS}}";
-
-  String $KRAKEN_DATA = "${kraken.data:#{environment.KRAKEN_DATA}}";
 
   String $KRAKEN_SSE_KEEP_ALIVE_DELAY = "${kraken.sse.keep-alive:#{environment.KRAKEN_SSE_KEEP_ALIVE_DELAY ?: 15}}";
 }
